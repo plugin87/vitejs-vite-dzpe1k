5d17d27c28f1a70dc3974f633fac130413cbff11
plugin87 <pannana1198@gmail.com>
2022-07-22 23:57:53 +0700

    3 files modified

--- src/components/pages/main.tsx
+++ src/components/pages/main.tsx
@@ -1,5 +1,24 @@
-const Main = () => {
-  return <div>Main</div>;
+import { Dispatch, FC, SetStateAction } from 'react';
+
+export interface MainProps {
+  setView: Dispatch<SetStateAction<any>>;
+}
+
+const Main: FC<MainProps> = ({ setView }) => {
+  return (
+    <div>
+      Main{' '}
+      <a
+        href="#"
+        onClick={(e) => {
+          e.preventDefault();
+          setView('login');
+        }}
+      >
+        Login
+      </a>
+    </div>
+  );
 };
 
 export default Main;
